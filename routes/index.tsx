d69e988cda3d0d7b5b26ef8ae71605f8c201d676
Kevin Whinnery <kevin.whinnery@gmail.com>
2023-06-02 17:04:31 -0500

    format

--- routes/index.tsx
+++ routes/index.tsx
@@ -14,8 +14,8 @@ export default function Home() {
           alt="the fresh logo: a sliced lemon dripping with juice"
         />
         <p class="my-6">
-          Welcome to `fresh`. Try updating this message in the ./routes/index.tsx
-          file, and refresh.
+          Welcome to `fresh`. Try updating this message in the
+          ./routes/index.tsx file, and refresh.
         </p>
         <Counter start={3} />
       </div>
