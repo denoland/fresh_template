e907bfcec13b3a2346e3674d24d7ac5a435a15cc
GitHub Action <action@github.com>
2023-08-30 14:08:54 +0000

    Init

--- --vscode/fresh.config.ts
+++ --vscode/fresh.config.ts
@@ -1,3 +1,6 @@
 import { defineConfig } from "$fresh/server.ts";
-
-export default defineConfig({});
+import twindPlugin from "$fresh/plugins/twind.ts"
+import twindConfig from "./twind.config.ts";
+export default defineConfig({
+  plugins: [twindPlugin(twindConfig)]
+});
--- --vscode/routes/_app.tsx
+++ --vscode/routes/_app.tsx
@@ -7,7 +7,6 @@ export default function App({ Component }: AppProps) {
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>--vscode</title>
-        <link rel="stylesheet" href="/styles.css" />
       </head>
       <body>
         <Component />
